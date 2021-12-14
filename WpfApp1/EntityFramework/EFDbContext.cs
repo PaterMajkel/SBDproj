d64fd79863e736b2a016db31e13a6e3727837e69
czarnaherbata <czarnaherbata223@gmail.com>
2021-12-14 21:32:07 +0100

    dodanie 2 brakujących okien
    Dodanie kartotekaWykroczenie oraz kartotekaPrzestępstwo oraz przejscie między panelami

--- WpfApp1/EntityFramework/EFDbContext.cs
+++ WpfApp1/EntityFramework/EFDbContext.cs
@@ -32,9 +32,10 @@ namespace EntityFramework
             public EFDbContext CreateDbContext(string[] args)
             {
                 var optionsBuilder = new DbContextOptionsBuilder<EFDbContext>();
-                optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=I:\\Filip\\SEMESTR5\\Projekt\\SBDproj\\WpfApp1\\EntityFramework\\Database1.mdf;Integrated Security=True");
+                optionsBuilder.UseSqlServer("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\czarn\\OneDrive\\Dokumenty\\GitHub\\SBDproj\\WpfApp1\\EntityFramework\\Database1.mdf;Integrated Security=True");
                 // MICHAŁ Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"F:\\student debil sem 5\\test\\SBDproj\\WpfApp1\\EntityFramework\\Policja.mdf\";Integrated Security=True
                 // FILIP Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=I:\\Filip\\SEMESTR5\\Projekt\\SBDproj\\WpfApp1\\EntityFramework\\Database1.mdf;Integrated Security=True
+                //Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\czarn\\OneDrive\\Dokumenty\\GitHub\\SBDproj\\WpfApp1\\EntityFramework\\Database1.mdf;Integrated Security=True
                 return new EFDbContext(optionsBuilder.Options);
             }
         }
